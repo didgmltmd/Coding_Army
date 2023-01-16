5abdb65858612bb19d0e3c545bf219bcc72b94ee
ballPen <gomboy11@naver.com>
2023-01-16 13:39:37 +0000

    js p.207 problem2 success

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+function People(name,tel,loc){
+  this.name = name;
+  this.tel = tel;
+  this.loc = loc;
 
-
-var book = [];
-function dataSet(){
-  book.push = ({name:"홍길동",tel:"010-111-1111",loc:"서울 강남구"});
-  book.push = ({name:"김두한",tel:"010-222-2222",loc:"서울 강서구"});
-  book.push = ({name:"이춘복",tel:"010-333-3333",loc:"부산 동구"});
-  book.push = ({name:"임꺽정",tel:"010-444-4444",loc:"전북 익산"});
-  book.push = ({name:"신나라",tel:"010-555-5555",loc:"서울 관악구"});  
-}
-var searchName = prompt("찾으실 이름을 입력하세요");
-
-dataSet();
-
-for(key in book){
-  if(book[key].name == searchName){
-    document.write("이름: ",book[key].name," 전화번호:",book[key].tel," 주소:",book[key].loc);
+  this.showInfor = function(){
+    document.write("이름: ",this.name,"<br/>","전화번호: ",this.tel,"<br/>","주소: ",this.loc);
   }
 }
+var searchName = prompt("찾고자하는 이름을 입력해주세요.");
 
+var peopleInfo = [];
+peopleInfo.push(new People("홍길동","010-111-1111","서울 강남구"));
+peopleInfo.push(new People("김두한","010-222-2222","서울 강서구"));
+peopleInfo.push(new People("이춘복","010-333-3333","부산 동구"));
+peopleInfo.push(new People("임꺽정","010-444-4444","전북 익산"));
+peopleInfo.push(new People("신나라","010-555-5555","서울 관악구"));
 
-
-
+for(let i = 0;i<5;i++){
+  if(peopleInfo[i].name == searchName){
+    peopleInfo[i].showInfor();
+  }
+}
